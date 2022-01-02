743f4bee323a093a9933871c520fcc82bc152ce2
janole <ole@mobileways.de>
2022-01-02 18:47:57 +0100

    "Refactor" the building script

--- index.js
+++ index.js
@@ -2,24 +2,33 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 import { getBarChart } from './util.js';
 
-const COUNTRIES = ["DEU", "FRA", "GBR", "ITA", "NLD", "ESP", "FIN", "AUT", "BEL", "DNK", "EST", "GRC", "HUN", "POL", "IRL", "PRT", "ROU", "SWE", "NOR", "CHE"];
+const COUNTRIES = ["DEU", "FRA", "GBR", "ITA", "NLD", "ESP", "FIN", "AUT", "BEL", "DNK", "EST", "GRC", "HUN", "POL", "IRL", "PRT", "ROU", "SWE", "NOR", "CHE", "BGR",];
 
 const src = "https://covid.ourworldindata.org/data/owid-covid-data.json";
 const dst = "owid-covid-data.json";
 
 // const stats = fs.statSync(dst);
 
-function process(data)
+function getChart({ data, left, right, sort, reverse, labelLeft, labelRight, })
 {
     const countries = COUNTRIES;
 
-    var maxLeft = 100, maxRight = 5000, scaleLeft = maxRight / maxLeft;
+    var maxLeft = 100, maxRight = 100, scaleLeft = maxRight / maxLeft;
 
     const chartData = countries.map(country =>
     {
         const timeline = data[country].data.sort((a, b) => b.date.localeCompare(a.date));
 
-        const valid = timeline.find(d => d.total_deaths_per_million > 0 && d.people_fully_vaccinated_per_hundred > 0 && d.total_boosters_per_hundred > 0);
+        const valid = timeline.find(d => left(d) > 0 && right(d) > 0);
+
+        const r = right(valid);
+
+        if (r > maxRight)
+        {
+            const high10 = Math.pow(10, parseInt(Math.log10(r)));
+            maxRight = (parseInt(r / high10) + 1) * high10;
+            scaleLeft = maxRight / maxLeft;
+        }
 
         return { id: country, ...valid, location: data[country].location, };
 
@@ -27,20 +36,55 @@ function process(data)
     {
         if (valid.date)
         {
-            arr.push({ id: valid.location, value: -valid.total_boosters_per_hundred * scaleLeft, type: 0, date: valid.date, });
-            arr.push({ id: valid.location, value: valid.total_deaths_per_million, type: 1, date: valid.date, });
+            arr.push({ ...valid, id: valid.location, value: -left(valid) * scaleLeft, title: left(valid), type: 0, date: valid.date, });
+            arr.push({ ...valid, id: valid.location, value: right(valid), type: 1, date: valid.date, });
         }
 
         return arr;
+
     }, []);
 
-    chartData.sort((a, b) => b.value - a.value);
-    // chartData.sort((a, b) => a.value - b.value);
+    if (sort == "right")
+        chartData.sort(((a, b) => right(b) - right(a)));
+    else
+        chartData.sort(((a, b) => left(b) - left(a)));
 
-    const chart = getBarChart({ chartData, xDomain: [-maxLeft * scaleLeft, maxRight], xFormat: d => d < 0 ? parseInt(-d / scaleLeft) + "%" : d, xLabelLeft: "Percentage of population fully vaccinated", xLabel: "vs.", xLabelRight: "Total deaths per million", });
-    fs.writeFileSync("test.svg", chart);
+    if (reverse)
+        chartData.reverse();
 
-    fs.writeFileSync("test.html", "<html><body>" + chart + "</body></html>");
+    const chart = getBarChart({ chartData, xDomain: [-maxLeft * scaleLeft, maxRight], xFormat: d => d < 0 ? parseInt(-d / scaleLeft) + "%" : d, xLabelLeft: labelLeft, xLabel: "vs.", xLabelRight: labelRight, });
+
+    return chart;
+}
+
+function process(data)
+{
+    const stats = [];
+
+    stats.push({
+        left: d => d?.people_fully_vaccinated_per_hundred,
+        right: d => d?.total_deaths_per_million,
+        labelLeft: "Percentage of population fully vaccinated",
+        labelRight: "Total deaths per million"
+    });
+
+    stats.push({
+        left: d => d?.people_fully_vaccinated_per_hundred,
+        right: d => d?.excess_mortality_cumulative_per_million,
+        labelLeft: "Percentage of population fully vaccinated",
+        labelRight: "Excess mortality per million"
+    });
+
+    const charts = [];
+
+    stats.forEach(stat =>
+    {
+        charts.push(getChart({ data, ...stat, reverse: true, }));
+        charts.push(getChart({ data, ...stat, sort: "right", }));
+    });
+
+    fs.mkdirSync("docs", { recursive: true });
+    fs.writeFileSync("docs/index.html", "<html><body>" + charts.join("<br />") + "</body></html>");
 }
 
 fs.readFile(dst, {}, (err, data) =>
@@ -48,9 +92,7 @@ fs.readFile(dst, {}, (err, data) =>
     process(JSON.parse(data));
 });
 
-/*
 fetch(src).then(res => res.json()).then(data =>
 {
     fs.writeFileSync(dst, JSON.stringify(data, null, 2));
 });
-*/
