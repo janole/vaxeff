7d6b3b447243082f534597ccf0bbed8f16aef6e3
janole <ole@mobileways.de>
2022-01-02 18:49:08 +0100

    Refactor the wrapper for the D3 bar

--- util.js
+++ util.js
@@ -7,7 +7,7 @@ const { JSDOM } = jsdom;
 function getBarChart({ chartData, xDomain, xFormat, xLabel, xLabelLeft, xLabelRight, })
 {
     const id = "chart";
-    const width = 1024, height = 1024;
+    const width = 1024, height = 768;
 
     const document = new JSDOM().window.document;
 
@@ -19,11 +19,6 @@ function getBarChart({ chartData, xDomain, xFormat, xLabel, xLabelLeft, xLabelRi
         .attr('class', 'svg-chart')
         .attr('width', width)
         .attr('height', height);
-    //        .append('g');
-    //        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-    //     .call(chart.render.bind(chart));
-
 
     const config =
     {
@@ -31,9 +26,10 @@ function getBarChart({ chartData, xDomain, xFormat, xLabel, xLabelLeft, xLabelRi
         x: d => d.value,
         y: d => d.id,
         z: d => d.type,
-        title: d => "X" + d.location,
+        title: d => d.title || d.value,
         xDomain,
         width,
+        height,
         xFormat,
         xLabel,
         xLabelLeft,
@@ -42,34 +38,10 @@ function getBarChart({ chartData, xDomain, xFormat, xLabel, xLabelLeft, xLabelRi
 
     new StackedBarChart(chartData, config,);
 
-    console.log("CHART", chartData, config,);
-
     const html = d3.select(document.getElementById(id)).node().innerHTML;
     d3.select(document.getElementById(id)).remove();
 
     return html;
-    /*
-    const document = new JSDOM().window.document;
-    global.document = document;
-
-    const body = d3.select(document.body).select("body");
-
-    const width = 1024, height = 768;
-
-    const svg = body.append("svg").attr("width", width).attr("height", height);
-
-    svg.append("line")
-        .attr("x1", 100)
-        .attr("y1", 100)
-        .attr("x2", 200)
-        .attr("y2", 200)
-        .style("stroke", "rgb(255,0,0)")
-        .style("stroke-width", 2);
-
-    const result = body.node().innerHTML;
-
-    return result;
-    */
 }
 
 export { getBarChart, };
